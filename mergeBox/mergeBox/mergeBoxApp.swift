28bd69cb7b72d4a0018dcdfdf032e2a17f89e9bd
松浦 <2210344@ecc.ac.jp>
2025-10-16 22:52:02 +0900

    add: 最初のコミット

---
+++ mergeBox/mergeBox/mergeBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  mergeBoxApp.swift
+//  mergeBox
+//
+//  Created by 松浦壮吾 on 2025/10/16.
+//
+
+import SwiftUI
+
+@main
+struct mergeBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
