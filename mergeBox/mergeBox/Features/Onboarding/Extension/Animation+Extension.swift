7452f230945395028eb88670facee4fd5d44efe1
松浦 <2210344@ecc.ac.jp>
2025-11-09 19:05:36 +0900

    add: 色々追加

---
+++ mergeBox/mergeBox/Features/Onboarding/Extension/Animation+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Animation+Extension.swift
+//  mergeBox
+//
+//  Created by 松浦壮吾 on 2025/11/08.
+//
+
+import Lottie
+
+extension LottieAnimation {
+    static let studyAnimation = named("study")
+    
+    static let smileAnimation = named("smile")
+    
+    static let loginAnimation = named("login")
+}
---
+++ mergeBox/mergeBox/Features/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingViewModel.swift
+//  mergeBox
+//
+//  Created by 松浦壮吾 on 2025/10/23.
+//
+
+
+import Observation
+
+@Observable
+class OnboardingViewModel {
+    var isLoading: Bool = false
+}
