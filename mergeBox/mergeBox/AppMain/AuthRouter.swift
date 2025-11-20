95a27c77ed1f0964b235c5316d7db8ac74f30e2f
松浦 <2210344@ecc.ac.jp>
2025-11-20 23:23:58 +0900

    feat: オンボーディングからログイン画面へ遷移するためのRouter作成

---
+++ mergeBox/mergeBox/AppMain/AuthRouter.swift
@@ -0,0 +1,32 @@
+//
+//  AuthRouter.swift
+//  mergeBox
+//
+//  Created by 松浦壮吾 on 2025/11/12.
+//
+
+import SwiftUI
+import Observation
+
+enum AuthRoute: Hashable {
+    case login
+}
+
+@Observable
+final class AuthRouter {
+    var path = NavigationPath()
+    
+    func navigate(to route: AuthRoute) {
+        path.append(route)
+    }
+    
+    func pop() {
+        if !path.isEmpty {
+            path.removeLast()
+        }
+    }
+    
+    func popToRoot() {
+        path = NavigationPath()
+    }
+}
